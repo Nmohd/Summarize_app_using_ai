50ff6774307b9918ebc01e559f2a51d919384e5f
Nmohd <najeebu50@gmail.com>
2023-07-19 08:41:20 +0530

    Changed error message

--- ai_summarizer/src/components/Demo.jsx
+++ ai_summarizer/src/components/Demo.jsx
@@ -119,7 +119,7 @@ const Demo = () => {
           <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
         ) : error ? (
           <p className="font-inter font-bold text-black text-center">
-            Well, that wasn't supposed to happen...
+            It may happen as the free API expired.....
             <br />
             <span className="font-satoshi font-normal text-gray-700">
               {error?.data?.error}
